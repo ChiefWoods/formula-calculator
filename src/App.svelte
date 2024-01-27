bd870c282456add9468a2aaaba2e035df0275ed1
Chii Yuen <chii.yuen@hotmail.com>
2024-01-27 23:39:09 +0800

    Complete styling

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,213 @@
-<script lang="ts"></script>
+<script lang="ts">
+  import Button from "./lib/KeypadBtn.svelte";
+  import Footer from "./lib/Footer.svelte";
+</script>
 
-<main></main>
+<main>
+  <section>
+    <div id="top">
+      <div id="head">
+        <p id="brand">WOODS</p>
+        <p id="model">SE-104</p>
+        <div id="solar">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+      <div id="screen">
+        <p id="input"></p>
+        <p id="display">0</p>
+      </div>
+    </div>
+    <div id="keypad">
+      <Button id="negative" className="negative" content="&#177;" />
+      <Button id="clear" className="erase" content="C" />
+      <Button id="backspace" className="erase" content="&#8592;" />
+      <Button id="divide" className="operator" content="&#247;" />
+      <Button id="seven" className="digit" content="7" />
+      <Button id="eight" className="digit" content="8" />
+      <Button id="nine" className="digit" content="9" />
+      <Button id="multiply" className="operator" content="&#215;" />
+      <Button id="four" className="digit" content="4" />
+      <Button id="five" className="digit" content="5" />
+      <Button id="six" className="digit" content="6" />
+      <Button id="subtract" className="operator" content="&#8722;" />
+      <Button id="one" className="digit" content="1" />
+      <Button id="two" className="digit" content="2" />
+      <Button id="three" className="digit" content="3" />
+      <Button id="add" className="operator" content="&#43;" />
+      <Button id="zero" className="digit" content="0" />
+      <Button id="decimal" className="digit" content="." />
+      <Button id="equals" className="operator" content="&#61;" />
+    </div>
+  </section>
+</main>
+<Footer />
 
-<style></style>
+<style>
+  @import url("https://fonts.googleapis.com/css2?family=Courier+Prime&display=swap");
+
+  @font-face {
+    font-family: Eurostile;
+    src: url("./assets/fonts/Eurostile/EurostileBold.ttf") format("truetype");
+    font-weight: bold;
+  }
+
+  @font-face {
+    font-family: DS-Digital;
+    src: url("./assets/fonts/ds_digital/DS-DIGI.TTF") format("truetype");
+  }
+
+  :root {
+    --top-gray: #c6c6ce;
+    --solar-gray: #362f33;
+    --screen-white: #d7e4e8;
+    --keypad-white: #eaeaea;
+    --font-gray: #44494e;
+    --calculator-border-gray: #8d929c;
+    --calculator-border-radius: 30px;
+  }
+
+  main {
+    flex: 1;
+    display: grid;
+    place-items: center;
+  }
+
+  #top {
+    display: flex;
+    flex-direction: column;
+    background: var(--top-gray);
+    border: 4px solid var(--calculator-border-gray);
+    border-radius: var(--calculator-border-radius)
+      var(--calculator-border-radius) 0 0;
+    border-bottom: none;
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 10px;
+  }
+
+  #head {
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-start;
+    gap: 15px;
+    padding-inline: 4px;
+    color: var(--font-gray);
+    width: 100%;
+  }
+
+  #brand {
+    font-family: Eurostile, monospace;
+    font-size: 2.5rem;
+    letter-spacing: 1.2px;
+  }
+
+  #model {
+    font-family: Eurostile, monospace;
+    font-size: 1.3rem;
+    font-weight: bold;
+    letter-spacing: 0.05rem;
+    white-space: nowrap;
+  }
+
+  #solar {
+    width: 120px;
+    height: 35px;
+    background-color: rgb(157, 157, 157);
+    border: 4px solid var(--solar-gray);
+    border-radius: 4px;
+    display: flex;
+    gap: 1px;
+    margin-left: auto;
+
+    & div {
+      background-color: var(--solar-gray);
+      width: 100%;
+      height: 100%;
+    }
+  }
+
+  #screen {
+    background-color: var(--screen-white);
+    height: 100px;
+    min-width: 300px;
+    width: 100%;
+    border-radius: 3px;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    justify-content: space-between;
+    padding: 8px 8px 0;
+  }
+
+  #input {
+    font-family: DS-Digital;
+    font-size: 2.7rem;
+    white-space: nowrap;
+  }
+
+  #display {
+    font-family: DS-Digital;
+    font-size: 5.5rem;
+    white-space: nowrap;
+  }
+
+  #keypad {
+    background: var(--keypad-white);
+    border: 4px solid var(--calculator-border-gray);
+    border-radius: 0 0 var(--calculator-border-radius)
+      var(--calculator-border-radius);
+    padding: 20px;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-template-rows: repeat(5, 50px);
+    gap: 12px;
+  }
+
+  @media (max-width: 450px) {
+    #top {
+      padding: 15px;
+    }
+
+    #solar {
+      width: 90px;
+    }
+
+    #solar > div:nth-child(4) {
+      display: none;
+    }
+
+    #screen {
+      min-width: 0;
+    }
+  }
+
+  @media (max-width: 400px) {
+    #model {
+      display: none;
+    }
+
+    #screen {
+      height: 90px;
+    }
+
+    #input {
+      font-size: 2.25rem;
+    }
+
+    #display {
+      font-size: 4.5rem;
+    }
+
+    #keypad {
+      padding: 15px;
+      gap: 6px;
+      grid-template-rows: repeat(5, 40px);
+    }
+  }
+</style>
---
+++ src/lib/KeypadBtn.svelte
@@ -0,0 +1,100 @@
+<script lang="ts">
+  export let id: string;
+  export let className: string;
+  export let content: string;
+</script>
+
+<button {id} class={className}>{content}</button>
+
+<style>
+  :root {
+    --key-white: #fffdfb;
+    --key-white-hover: #f5f5f5;
+    --key-white-active: #ebebeb;
+    --key-orange: #db844d;
+    --key-orange-hover: #d17a44;
+    --key-orange-active: #c26e3c;
+    --key-blue: #41558a;
+    --key-blue-hover: #3a4d7f;
+    --key-blue-active: #334575;
+    --key-grey: #555859;
+    --key-grey-hover: #4d4f52;
+    --key-grey-active: #45474a;
+  }
+
+  button {
+    cursor: pointer;
+    font-family: 'Courier Prime', monospace;
+    font-size: 3rem;
+    border-radius: 6px;
+    color: var(--key-white);
+  }
+
+  #negative {
+    background: var(--key-white);
+    color: var(--key-blue);
+  }
+
+  #negative:hover {
+    background: var(--key-white-hover);
+  }
+
+  #negative:active, #negative.active {
+    background: var(--key-white-active);
+  }
+
+  .erase {
+    background: var(--key-orange);
+    color: var(--key-white);
+  }
+
+  .erase:hover {
+    background: var(--key-orange-hover);
+  }
+
+  .erase:active, .erase.active {
+    background: var(--key-orange-active);
+  }
+
+  .operator {
+    background: var(--key-blue);
+    color: var(--key-white);
+  }
+
+  .operator:hover {
+    background: var(--key-blue-hover);
+  }
+
+  .operator:active, .operator.active {
+    background: var(--key-blue-active);
+  }
+
+  .digit {
+    background: var(--key-grey);
+    color: var(--key-white);
+  }
+
+  .digit:hover {
+    background: var(--key-grey-hover);
+  }
+
+  .digit:active, .digit.active {
+    background: var(--key-grey-active);
+  }
+
+  #add {
+    grid-area: 4 / 4 / 6 / 5;
+  }
+
+  @media (max-width: 450px) {
+    button {
+      font-size: 2.75rem;
+    }
+  }
+
+  @media (max-width: 400px) {
+    button {
+      font-size: 2.5rem;
+    }
+  }
+</style>
